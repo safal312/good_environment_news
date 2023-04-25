d96c9f4f6f4c8a435ae920d7ef583cef78651185
Safal <safal.bp2@gmail.com>
2023-04-25 23:56:24 +0400

    added scraper for nyt environment and energy topic and environment health news biodiversity section

--- news_scraper/NewsScraper.py
+++ news_scraper/NewsScraper.py
@@ -16,6 +16,67 @@ class NewsScraper:
             scores = self.sia.polarity_scores(all_text)
             self.scraped_data[i]["scores"] = scores
         return self
+
+    def scrape_nyt_energy_environment(self):
+        response = requests.get('https://www.nytimes.com/section/business/energy-environment')
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        main = soup.find("section", {"id": "stream-panel"})
+        articles = main.find_all("li")
+
+        data = []
+
+        for item in articles:
+            article = {"title": "", "link": "", "body": "", "date": "", "image": "",
+                "scores": {}, "source": "NYT"}
+            
+            try:
+                article['date'] = item.find_all("span")[-1].text
+                article['link'] = "https://www.nytimes.com" + item.find("a").get("href")
+                article['title'] = item.find("h2").text
+                article['image'] = item.find("img").get("src")
+                article['body'] = item.find("p").text
+            except:
+                print("Skipping faulty article in NYT Energy and Environment")
+                continue
+            data.append(article)
+
+        self.scraped_data.extend(data)
+
+        return self
+            
+    def scrape_ehn(self):
+        response = requests.get('https://www.ehn.org/biodiversity/')
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        items = soup.find_all("div", {"class": "widget"})
+
+        data = []
+
+        for item in items:
+            article = {"title": "", "link": "", "body": "", "date": "", "image": "",
+                "scores": {}, "source": "EHN"}
+            
+            try:
+                head = item.find("div", {"class": "row"}).find("div", {"class": "widget__head"})
+
+                article['link'] = "https://aljazeera.com" + head.find("a").get("href")
+                article['title'] = item.find(class_="widget__headline").text.strip()
+                # article['title'] = item.find("h4", {"class": "widget__headline"}).text.strip()
+                article['image'] = "https://aljazeera.com" + item.find("img", {"role": "img"}).get("data-runner-src")
+    
+                # body = item.find("div", {"class": "widget__body"})
+                article['date'] = item.find("div", {"class": "social-date"}).text.strip()
+                article['body'] = item.find("div", {"class": "body-description"}).text.strip()
+
+            except:
+                print("Skipping faulty article in EHN")
+                continue
+            data.append(article)
+
+        self.scraped_data.extend(data)
+
+        return self
     
     def scrape_aljazeera(self):
         response = requests.get('https://www.aljazeera.com/tag/environment/')
@@ -45,10 +106,9 @@ class NewsScraper:
                 article['image'] = date.get("src")
 
                 data.append(article)
-            self.scraped_data = data
+            self.scraped_data.extend(data)
         except:
             print("Exception in Scraping Aljazeera")
-            self.scraped_data = []
         
         return self
             
--- news_scraper/main.py
+++ news_scraper/main.py
@@ -8,11 +8,16 @@ from NewsScraper import NewsScraper
 load_dotenv()
 
 client = pymongo.MongoClient(os.getenv("CONNECTION_STRING"))
-
-aljazeera_latest = client.newsdb.aljazeera_latest
+latest = client.newsdb.latest
 
 ns = NewsScraper()
-aljazeera_articles = ns.scrape_aljazeera().add_sentiment_scores().get_data()
-# aljazeera_latest.insert_many(aljazeera_articles)
+latest.delete_many({})      # clear out old news
+
+aljazeera_articles = ns.scrape_aljazeera().add_sentiment_scores()
+ehn_climate = ns.scrape_ehn().add_sentiment_scores()
+nyt = ns.scrape_nyt_energy_environment().add_sentiment_scores()
+print(ns.get_data())
+
+latest.insert_many(ns.get_data())
 
 # scrape gulf news - middle east, environment for UAE https://gulfnews.com/uae/environment
