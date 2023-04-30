532842c86bfca75d6d991947811952fdb7d71333
Safal <safal.bp2@gmail.com>
2023-05-01 02:55:39 +0400

    added scraper for the national, added check for greenwashing, added alert, added bg color in cards, added dashboard to modal

--- news_scraper/NewsScraper.py
+++ news_scraper/NewsScraper.py
@@ -1,45 +1,143 @@
 import requests
 from bs4 import BeautifulSoup
 from nltk.sentiment import SentimentIntensityAnalyzer
+import subprocess
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class Article(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setdefault("title", "")
+        self.setdefault("link", "")
+        self.setdefault("body", "")
+        self.setdefault("date", "")
+        self.setdefault("image", "")
+        self.setdefault("scores", {})
+        self.setdefault("keywords", [])
+        self.setdefault("greenwashing", 0)
+        self.setdefault("source", "")
 
 class NewsScraper:
     def __init__(self):
         self.sia = SentimentIntensityAnalyzer()
         self.scraped_data = []
+        self.options = Options()
+        # self.options.add_argument('--headless=new')
+        # self.options.add_argument('--no-sandbox')
+        # self.options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
+        self.greenwords = []
     
+    def get_greenwords(self):
+        with open("greenwashing.csv", "r") as f:
+            # print(f.readlines())
+            for line in f.readlines():
+                line = line.strip()
+                if len(line) > 2:
+                    self.greenwords.extend(line.split(" "))
+                else:
+                    self.greenwords.append(line)
+
     def get_data(self):
         return self.scraped_data
 
     def add_sentiment_scores(self):
+        if not len(self.greenwords) > 0: self.get_greenwords()
+
         for i in range(len(self.scraped_data)):
-            all_text = self.scraped_data[i]['title'] + " " + self.scraped_data[i]['body']
+            title = self.scraped_data[i]['title']
+            body = self.scraped_data[i]['body']
+            if not title: title= ""
+            if not body: body = ""
+            all_text = title + " " + body
             scores = self.sia.polarity_scores(all_text)
+
+            if scores["compound"] > 0:
+                title_words = title.split(" ")
+                for word in title_words:
+                    if word in self.greenwords:
+                        self.scraped_data[i]["greenwashing"] = 1
+                        self.scraped_data[i]["keywords"].append("greenwashing")
+                        break
+
             self.scraped_data[i]["scores"] = scores
+        return self
+    
+    def scrape_the_national(self):
+        driver = self.driver.get("https://www.thenationalnews.com/climate/environment/")
+
+        stories = self.driver.find_elements(By.CLASS_NAME, "story-card")
+        list_items = []
+        try:
+            list_items = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "list-item")))
+        except:
+            print("Couldn't get listed items in The National")
+
+        data = []
+
+        for item in stories:
+            try:
+                article = Article()
+                
+                article['link'] = item.find_element(By.TAG_NAME, "a").get_attribute("href")
+                article['image'] = item.find_element(By.TAG_NAME, "img").get_attribute("src")
+                article['title'] = item.find_element(By.CLASS_NAME, "story-card-headline").text
+                article['keywords'].append(item.find_element(By.TAG_NAME, "h3").text.lower())
+                article['keywords'].extend(["environment", "uae"])
+                article['source'] = "The National"
+                
+                data.append(article)
+            except:
+                print("Issue in The National article")
+
+        for item in list_items:
+            try:
+                article = Article()
+
+                article['link'] = item.find_element(By.TAG_NAME, "a").get_attribute("href")
+                article['title'] = item.find_element(By.TAG_NAME, "h2").text
+                article['image'] = item.find_element(By.TAG_NAME, "img").get_attribute("src")
+                article['keywords'].extend(["environment","uae"])
+                article['source'] = "The National"
+                
+                data.append(article)
+            except:
+                print("Issue in The National Article")
+        
+        self.scraped_data.extend(data)
         return self
 
     def scrape_nyt_energy_environment(self):
-        response = requests.get('https://www.nytimes.com/section/business/energy-environment')
-        soup = BeautifulSoup(response.text, 'html.parser')
 
-        main = soup.find("section", {"id": "stream-panel"})
-        articles = main.find_all("li")
+        self.driver.get("https://www.nytimes.com/section/business/energy-environment")
+
+        main = self.driver.find_element(By.ID, "stream-panel")
+        articles = main.find_elements(By.TAG_NAME, "li")
 
         data = []
 
         for item in articles:
-            article = {"title": "", "link": "", "body": "", "date": "", "image": "",
-                "scores": {}, "source": "NYT"}
+            article = Article()
             
             try:
-                article['date'] = item.find_all("span")[-1].text
-                article['link'] = "https://www.nytimes.com" + item.find("a").get("href")
-                article['title'] = item.find("h2").text
-                article['image'] = item.find("img").get("src")
-                article['body'] = item.find("p").text
+                article['date'] = item.find_elements(By.TAG_NAME, "span")[-1].text
+                article['link'] = "https://www.nytimes.com" + item.find_element(By.TAG_NAME, "a").get_attribute("href")
+                article['title'] = item.find_element(By.TAG_NAME, "h2").text
+                article['image'] = item.find_element(By.TAG_NAME, "img").get_attribute("src")
+                article['body'] = item.find_element(By.TAG_NAME, "p").text
+                article['keywords'].extend(["environment", "energy", "business", "nyt"])
+                article['source'] = "New York Times"
+
+                data.append(article)
             except:
                 print("Skipping faulty article in NYT Energy and Environment")
-                continue
-            data.append(article)
 
         self.scraped_data.extend(data)
 
@@ -54,20 +152,20 @@ class NewsScraper:
         data = []
 
         for item in items:
-            article = {"title": "", "link": "", "body": "", "date": "", "image": "",
-                "scores": {}, "source": "EHN"}
+            article = Article()
             
             try:
                 head = item.find("div", {"class": "row"}).find("div", {"class": "widget__head"})
 
-                article['link'] = "https://aljazeera.com" + head.find("a").get("href")
+                article['link'] = head.find("a").get("href")
                 article['title'] = item.find(class_="widget__headline").text.strip()
                 # article['title'] = item.find("h4", {"class": "widget__headline"}).text.strip()
-                article['image'] = "https://aljazeera.com" + item.find("img", {"role": "img"}).get("data-runner-src")
+                article['image'] = item.find("img", {"role": "img"}).get("data-runner-src")
     
                 # body = item.find("div", {"class": "widget__body"})
                 article['date'] = item.find("div", {"class": "social-date"}).text.strip()
                 article['body'] = item.find("div", {"class": "body-description"}).text.strip()
+                article['source'] = "Environment Health News"
 
             except:
                 print("Skipping faulty article in EHN")
@@ -88,13 +186,12 @@ class NewsScraper:
 
         try:
             for item in items:
-                article = {"title": "", "link": "", "body": "", "date": "", "image": "",
-                 "scores": {}, "source": "Aljazeera"}
+                article = Article()
 
                 gc = item.find("div", {"class": "gc__content"})
                 title_el = gc.find("div", {"class": "gc__header-wrap"})
                 article['title'] = title_el.find("h3", {"class": "gc__title"}).find("span").text.replace("\xad", "")
-                article['link'] = title_el.find("a").get("href")
+                article['link'] = "https://aljazeera.com" + title_el.find("a").get("href")
 
                 desc = gc.find("div", {"class": "gc__body-wrap"})
                 article['body'] = desc.text.replace("\xad", "")
@@ -103,7 +200,8 @@ class NewsScraper:
                 article['date'] = date.text.replace("\xad", "")
 
                 date = item.find("img")
-                article['image'] = date.get("src")
+                article['image'] = "https://aljazeera.com" + date.get("src")
+                article["source"] = "Aljazeera"
 
                 data.append(article)
             self.scraped_data.extend(data)
--- news_scraper/main.py
+++ news_scraper/main.py
@@ -16,8 +16,8 @@ latest.delete_many({})      # clear out old news
 aljazeera_articles = ns.scrape_aljazeera().add_sentiment_scores()
 ehn_climate = ns.scrape_ehn().add_sentiment_scores()
 nyt = ns.scrape_nyt_energy_environment().add_sentiment_scores()
-print(ns.get_data())
-
+thenational = ns.scrape_the_national().add_sentiment_scores()
+# print(len(ns.get_data()))
 latest.insert_many(ns.get_data())
 
 # scrape gulf news - middle east, environment for UAE https://gulfnews.com/uae/environment
